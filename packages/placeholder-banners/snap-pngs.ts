629fedadcb9001c60380b178fc5db884c2005a6c
Hiroki Osame <hiroki.osame@gmail.com>
2023-08-08 23:44:12 +0900

    wip

---
+++ packages/placeholder-banners/snap-pngs.ts
@@ -0,0 +1,85 @@
+import { launch } from 'chrome-launcher';
+import CDP from 'chrome-remote-interface';
+import fs from 'fs/promises';
+import { createServer } from 'vite';
+
+const screenshotNode = async (
+	client: CDP.Client,
+	nodeId: number,
+) => {
+	const { model } = await client.DOM.getBoxModel({ nodeId });
+	const [x, y] = model.border;
+	const screenshot = await client.Page.captureScreenshot({
+		captureBeyondViewport: true,
+		clip: {
+			x,
+			y,
+			width: model.width,
+			height: model.height,
+			scale: 1,
+		},
+	});
+
+	return Buffer.from(screenshot.data, 'base64');
+};
+
+(async () => {
+	// tsx bugs out on this. Need to come back to it
+	const server = await createServer({
+		configFile: false,
+		root: __dirname,
+		server: {
+			port: 1337,
+		},
+	});
+	await server.listen();
+
+	const chrome = await launch({
+		chromeFlags: [
+			'--headless',
+			'--disable-gpu',
+		],
+	});
+
+	const browserClient = await CDP({
+		port: chrome.port,
+	});
+
+	const { targetId } = await browserClient.Target.createTarget({
+		url: 'http://localhost:5173',
+	});
+
+	const tabClient = await CDP({
+		port: chrome.port,
+		target: targetId,
+	});
+
+	await tabClient.Page.enable();
+	await tabClient.Page.loadEventFired();
+
+	// Set transparent bg for screenshot
+	tabClient.Emulation.setDefaultBackgroundColorOverride({
+		color: {
+			r: 0, g: 0, b: 0, a: 0,
+		},
+	});
+
+	const { root } = await tabClient.DOM.getDocument();
+	const banners = await tabClient.DOM.querySelectorAll({
+		nodeId: root.nodeId,
+		selector: '.banner',
+	});
+
+	for (const banner of banners.nodeIds) {
+		const { attributes } = await tabClient.DOM.getAttributes({ nodeId: banner });
+		const idIndex = attributes.findIndex((attr) => attr === 'id');
+		const id = attributes[idIndex + 1];
+
+		const snapshot = await screenshotNode(tabClient, banner);
+		await fs.writeFile(`./pngs/${id}.png`, snapshot);
+	}
+
+	await tabClient.close();
+	await browserClient.close();
+	await chrome.kill();
+})();
---
+++ packages/placeholder-banners/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import PlaceholderBanner from './PlaceholderBanner.vue';
+</script>
+
+<template>
+	<div class="flex flex-col gap-4">
+		<PlaceholderBanner
+			id="platinum-light"
+			tier="platinum"
+		/>
+
+		<PlaceholderBanner
+			id="platinum-dark"
+			mode="dark"
+			tier="platinum"
+		/>
+
+		<PlaceholderBanner
+			id="gold-light"
+			tier="gold"
+		/>
+
+		<PlaceholderBanner
+			id="gold-dark"
+			mode="dark"
+			tier="gold"
+		/>
+
+		<PlaceholderBanner
+			id="silver-light"
+			tier="silver"
+		/>
+
+		<PlaceholderBanner
+			id="silver-dark"
+			mode="dark"
+			tier="silver"
+		/>
+	</div>
+</template>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
