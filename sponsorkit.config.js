b02ea59c9b8ccb539613e4a49198fd2ba6ffc71a
Hiroki Osame <hiroki.osame@gmail.com>
2024-11-02 12:16:19 +0900

    chore: prorate sponsors

--- sponsorkit.config.js
+++ sponsorkit.config.js
@@ -7,59 +7,46 @@ export default defineConfig({
 		login: 'privatenumber',
 	},
 
-	renders: [
+	name: 'sponsors',
+	width: 800,
+	formats: ['svg'],
+
+	prorateOnetime: true,
+
+	tiers: [
 		{
-			name: 'sponsors',
-			width: 800,
-			formats: ['svg'],
-			tiers: [
-				{
-					title: 'Past Sponsors',
-					monthlyDollars: -1,
-					preset: tierPresets.xs,
-				},
-				{
-					title: 'Donors',
-					preset: tierPresets.xs,
-				},
-				{
-					title: 'Sponsors',
-					monthlyDollars: 2,
-					preset: tierPresets.base,
-				},
-				{
-					title: 'Bronze Sponsors',
-					monthlyDollars: 10,
-					preset: tierPresets.medium,
-				},
-				{
-					title: 'Silver Sponsors',
-					monthlyDollars: 25,
-					preset: tierPresets.large,
-				},
-				{
-					title: 'Gold Sponsors',
-					monthlyDollars: 100,
-					preset: tierPresets.large,
-				},
-				{
-					title: 'Premium Sponsors',
-					monthlyDollars: 300,
-					preset: tierPresets.xl,
-				},
-			],
+			title: 'Past Sponsors',
+			monthlyDollars: -1,
+			preset: tierPresets.xs,
 		},
 		{
-			name: 'list',
-			width: 640,
-			formats: ['svg'],
-			includePastSponsors: true,
-			includePrivate: true,
-			tiers: [
-				{
-					preset: tierPresets.xs,
-				},
-			],
+			title: 'Donors',
+			preset: tierPresets.xs,
+		},
+		{
+			title: 'Sponsors',
+			monthlyDollars: 2,
+			preset: tierPresets.base,
+		},
+		{
+			title: 'Bronze Sponsors',
+			monthlyDollars: 10,
+			preset: tierPresets.medium,
+		},
+		{
+			title: 'Silver Sponsors',
+			monthlyDollars: 25,
+			preset: tierPresets.large,
+		},
+		{
+			title: 'Gold Sponsors',
+			monthlyDollars: 100,
+			preset: tierPresets.large,
+		},
+		{
+			title: 'Premium Sponsors',
+			monthlyDollars: 300,
+			preset: tierPresets.xl,
 		},
 	],
 });
